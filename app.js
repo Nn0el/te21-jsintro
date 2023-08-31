ef3c4fe3c5f2397b5431abe36f62cefe3ad7df5b
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2023-08-31 11:01:19 +0200

    ✨

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log("Hello world!")
+
+let firstname
+firstname = "Karl"
+let lastname ="Den sextonde gustav"
+
+let num = 202 
+
+console.log(typeof(num))
+const namn = "bob"
+console.log(namn)
+
+for(let i = 0; i<9;i++){
+    console.log(namn)
+}
+
+var skola = "Nti på gud"
+console.log(skola)
+
+let a = "Noel"
+let b ='Noel'
+let c = `Mitt namn är ${a}`
+let d = 'Mitt namn är ' + b
+console.log(c)
+console.log(d)
+
+let e = 12 
+let f = 16.8
+let g = true
+let h = false
+console.log(e, f)

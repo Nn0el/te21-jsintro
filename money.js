b604c7010ad12555c6ac148539f455be4ec618ee
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2023-09-05 11:20:59 +0200

    Exchange rates

--- money.js
+++ money.js
@@ -2,3 +2,13 @@ let amount =100;
 let exchangeRate = 11.09;
 
 console.log(amount * exchangeRate)
+
+let exchangeRates = [
+    0.345,
+    1.12, 
+    9.456, 
+]
+exchangeRates.forEach(ex =>{
+    console.log(amount*ex)
+}
+    )
